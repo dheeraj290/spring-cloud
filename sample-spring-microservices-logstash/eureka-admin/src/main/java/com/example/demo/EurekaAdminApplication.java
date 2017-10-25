66e5cc91a75fe8da2c99fcfe85e7630cc918cea7
dev-dksc102876 <dev-dksc102876@dcsg.com>
2017-10-25 12:10:14 -0400

    temp changes done

--- sample-spring-microservices-logstash/eureka-admin/src/main/java/com/example/demo/EurekaAdminApplication.java
+++ sample-spring-microservices-logstash/eureka-admin/src/main/java/com/example/demo/EurekaAdminApplication.java
@@ -14,6 +14,7 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 public class EurekaAdminApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(EurekaAdminApplication.class, args);
 	}
 }
